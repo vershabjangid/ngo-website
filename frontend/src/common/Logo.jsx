46d87b0f1e61f2d7938fcf4453207f389ede4478
vershabjangid <vershabjangid@yahoo.com>
2025-09-01 13:10:12 +0530

    hl

--- frontend/src/common/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import logo from '../images/cropped-logo.png'
-export function Logo() {
-    return (
-        <>
-            <img src={logo} alt="" className='w-[100%]' />
-        </>
-    )
-}
--- frontend/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", ".flowbite-react\\class-list.json"],
-  theme: {
-    extend: {},
-  },
-}
